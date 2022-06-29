8d8c5d40fe48e86d766845a205510cf7b345861c
waki <79116630+yoeiwaki@users.noreply.github.com>
2022-06-30 01:56:00 +0900

    make select

--- chrome/js/waki.js
+++ chrome/js/waki.js
@@ -1,4 +1,8 @@
 let body = document.querySelector("body");
-let addElement = document.createElement("p");
-addElement.innerHTML = "button";
-body.prepend(addElement);
+let selectarea = document.querySelector(".showmore");
+let selectcourse = document.getElementsByName("select");
+let addElement = document.createElement("select");
+addElement.innerHTML = "<option>1Q</option> <option>2Q</option> <option>3Q</option> <option>4Q</option>";
+console.log(selectarea);
+console.log(selectcourse[0]);
+selectarea.insertBefore(addElement, selectcourse[0]);
