f0cae51b8bd2c29f79c27c782e1c2bfad863e91b
yoeiwaki <yoooa.w@gmail.com>
2022-07-01 15:05:08 +0900

    add style

--- chrome/js/waki.js
+++ chrome/js/waki.js
@@ -3,6 +3,9 @@ let selectarea = document.querySelector(".showmore");
 let selectcourse = document.getElementsByName("select");
 let addElement = document.createElement("select");
 addElement.innerHTML = "<option>1Q</option> <option>2Q</option> <option>3Q</option> <option>4Q</option>";
+addElement.style.marginRight = "0.3em";
 console.log(selectarea);
 console.log(selectcourse[0]);
 selectarea.insertBefore(addElement, selectcourse[0]);
+
+//margin-right:0.3em
