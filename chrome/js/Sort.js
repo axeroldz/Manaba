d024fcf9039de398ec28a975f09f4d0fb8e53a1e
waki <79116630+yoeiwaki@users.noreply.github.com>
2022-08-10 16:06:08 +0900

    waki.jsの内容をsort.jsに移動

--- chrome/js/Sort.js
+++ chrome/js/Sort.js
@@ -1,3 +1,15 @@
+function showSelect(){
+  let body = document.querySelector("body");
+  let selectarea = document.querySelector(".showmore");
+  let selectcourse = document.getElementsByName("select");
+  let addElement = document.createElement("select");
+  addElement.innerHTML = "<option>1Q</option> <option>2Q</option> <option>3Q</option> <option>4Q</option>";
+  addElement.style.marginRight = "0.3em";
+  //console.log(selectarea);
+  //console.log(selectcourse[0]);
+  selectarea.insertBefore(addElement, selectcourse[0]);
+}
+showSelect();
 (() => {
   var e = {
       484: function (e) {
