fbefd4fd337d27e68ae7db6b78e61787ed5d9720
David.K <davidkwan1276@gmail.com>
2022-09-06 15:56:59 +0900

    追加機能

--- chrome/js/content.js
+++ chrome/js/content.js
@@ -1,51 +1,55 @@
 (async ()=>{
     const delay = ms => new Promise(res => setTimeout(res, ms));
-    await delay(3000);
-    const readKamoku = () =>
+    
+    const courses = [];
+    const fetchQuarter = async () =>{
+    const link = 'https://manaba.ryukoku.ac.jp/s/home_course?chglistformat=thumbnail';
+    const result = await fetch(link);
+    const htmlText = await result.text();
+    const domparser = new DOMParser();
+    const doc = domparser.parseFromString(htmlText, 'text/html');
+    Array.from(doc.querySelectorAll('.course-list > li > a > table > tbody > tr > td')).map((a) => {
+        let _a, _b;
+        courses[(_a = a.querySelector('h3')) === null || _a === void 0 ? void 0 : _a.innerText.replace(/\s+/g, ' ').split(" ")[1]]=(_b = a
+                .querySelector('.info1')) === null || _b === void 0 ? void 0 : _b.innerText.replace(/\s+/g, ' ').split(" ")[1]
+        });
+    };
+    const insertClass = (courseName,quarter) =>{
+        //親ノード(courseName)のクラス名にquarterを追加する
+        if(quarter === '前期')
+            quarter = "1Q 2Q";
+        if(quarter === '後期')
+            quarter = "3Q 4Q";
+        const cell = courseName.parentNode;
+        const newName = cell.className +" "+ quarter;
+        cell.className = newName;
+    };
+    fetchQuarter();
+    await delay(800);
+    const readNInsertKamoku = () =>
     {
-        const table = document.getElementsByClassName("course");
-        let kamoku = ""
+        const table = Array.from(document.querySelectorAll('.course > .courselistweekly-c >a:not(.courseweekly-fav)'));
         for(let i = 0;i<table.length;i++){
-            let children = table[i].childNodes.length;
-            switch(children){
-                case 3:
-                    kamoku = table[i].childNodes[1].innerText.split(" ")[0];
-                    console.log(kamoku + " Q1 Q2 \n");
-                    break;
-                
-                case 5:
-                    kamoku = table[i].childNodes[1].innerText.split(" ")[0];
-                    console.log(kamoku + " Q1\n");
-                    kamoku = table[i].childNodes[3].innerText.split(" ")[0];
-                    console.log(kamoku + " Q2\n");
-                    break;
-                
-                case 7:
-                    kamoku = table[i].childNodes[1].innerText.split(" ")[0];
-                    console.log(kamoku + " Q1\n");
-                    kamoku = table[i].childNodes[3].innerText.split(" ")[0];
-                    console.log(kamoku + " Q2\n");
-                    kamoku = table[i].childNodes[5].innerText.split(" ")[0];
-                    console.log(kamoku + " Q3\n");
-                    break;
-
-                case 9:
-                    kamoku = table[i].childNodes[1].innerText.split(" ")[0];
-                    console.log(kamoku + " Q1\n");
-                    kamoku = table[i].childNodes[3].innerText.split(" ")[0];
-                    console.log(kamoku + " Q2\n");
-                    kamoku = table[i].childNodes[5].innerText.split(" ")[0];
-                    console.log(kamoku + " Q3\n");
-                    kamoku = table[i].childNodes[7].innerText.split(" ")[0];
-                    console.log(kamoku + " Q4\n");
-                    break;
-            }
-
+            let code = table[i].innerText.split(" ")[1];
+            if(Object.hasOwn(courses, code))
+                insertClass(table[i],courses[code])
         }
+    };
+    readNInsertKamoku();
+    
+    const search = (quarter) =>{
+        const cells =  Array.from(document.querySelectorAll('.course > .courselistweekly-c'));
+        cells.forEach( cell =>{
+            // cell は1科目の枠を示す。cell変数を使うといい。
+            if(cell.className.includes(quarter))
+                console.log(cell.querySelector('a').innerText);
+        })
     }
-    //WreadKamoku();
-    const table = document.getElementsByClassName("groupthreadlist")[0];
+    search('2Q'); //検索できるのが 1Q,2Q,3Q,4Q
+
+
     const addReport = async () => {
+        const table = document.getElementsByClassName("groupthreadlist")[0];
         if(table){
             const tableRow = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
             const reports = [];
@@ -53,7 +57,7 @@
                 for(let i = 0;i<tableRow.length;i++){
                     const row = tableRow[i];
                     const reportName = row.getElementsByTagName("td")[1].getElementsByTagName("div")[0].title.split(" ")[0]
-                    if(!reportName.includes("e-Learning")){
+                    if(!reportName.includes("自学自習")){
                         const reportLink = row.getElementsByTagName("th")[0].getElementsByTagName("a")[0].href;
                         let reportDate = row.getElementsByTagName("td")[0].title.split(" ")[0];
                         reportDate = reportDate.replaceAll('-','/');
